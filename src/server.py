84e687d638430f6106edbfc44b60e2c2a2834794
robert <xdobryr@stuba.sk>
2023-03-10 19:32:49 +0100

    add src/ and output.txt

---
+++ src/server.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__, template_folder="templates")
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
