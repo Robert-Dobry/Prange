5e6aaa60c19cfb641feeeb64e992b99a791a9b32
Robert-Dobry <xdobryr@stuba.sk>
2023-03-12 18:45:56 +0100

    invert matrix
    cleaar
    
    ;

--- src/main.py
+++ src/main.py
@@ -7,8 +7,28 @@ def mask_matrix(m, inf_set):
     return result
 
 
+def hwt(v):
+    result = 0
+    for item in v:
+        if item>0:
+            result+=1
+    return result
+
+
+def inverse_matrix(m):
+    det = int(numpy.linalg.det(m))
+    if det == 0:
+        return -1
+    else:
+        result = numpy.linalg.inv(m)
+        return result
+
+
 def mat_mul(a,b):
-    result = numpy.dot(a,b)
+    x = numpy.dot(a,b)
+    result = []
+    for item in x:
+        result.append(item%2)
     return result
 
 
@@ -94,7 +114,9 @@ e = gen_random_e(n,w)
 p = add_vectors(r,e)
 masked_m=mask_matrix(m,inf_set)
 masked_r=mask_vector(p,inf_set)
+inverse_masked_m=inverse_matrix(masked_m)
 x = mat_mul(masked_r, masked_m)
+xG = mat_mul(x, m)
 
 print(f"PRANGE'S ISD for n={n}, k={k}, w={w}")
 print('\ninf set:')
@@ -111,5 +133,10 @@ print('\nmasked g:')
 print_matrix(masked_m)
 print('\nmasked r:')
 print_set(masked_r)
+print('\ninverse masked g:')
+if type(inverse_masked_m) == type([]):
+    print_matrix(inverse_masked_m)
+else:
+    print('matrix is not invertible')
 print('\nx:')
 print_set(x)
