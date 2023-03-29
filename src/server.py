cca89b270bde3230299edb54799d4e17b28ffb87
Robert Dobry <robert.dobry@kistler.com>
2023-03-29 15:55:42 +0200

    adjust for big code

--- src/server.py
+++ src/server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from func import generate_matrix, gen_random_codeword, gen_random_e, add_vectors
+from func import generate_matrix, gen_random_codeword, gen_random_e, add_vectors, mask_matrix, mask_vector, gen_information_set
 
 app = Flask(__name__, template_folder="templates")
 
@@ -17,14 +17,25 @@ def generate():
         matrix_data = generate_matrix(n,k)
         matrix={"data" : matrix_data}
 
+        i_data = gen_information_set(n,k)
+        i = {"data" : i_data}
+
         m_data = gen_random_codeword(matrix["data"])
         m={"data": m_data}
 
         e_data = gen_random_e(n,1)
         e = {"data" : e_data}
+
         r_data = add_vectors(m_data, e_data)
         r = {"data" : r_data}
-        return render_template('matrix.html', matrix=matrix, m=m, e=e, r=r)
+
+        masked_r_data = mask_vector(r_data, i_data)
+        masked_r = {"data" : masked_r_data}
+
+        masked_matrix_data = mask_matrix(matrix_data,i_data)
+        masked_matrix={"data" : masked_matrix_data}
+
+        return render_template('matrix.html', matrix=matrix, i=i,m=m, e=e, r=r, masked_matrix=masked_matrix, masked_r=masked_r, )
     else:
         return render_template('index.html')
 
