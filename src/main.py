ac5cef29915c0a9c90856b4e06ef4cea41de38fd
Robert-Dobry <xdobryr@stuba.sk>
2023-04-27 00:57:55 +0200

    another stuff

--- src/main.py
+++ src/main.py
@@ -2,31 +2,36 @@ import func as f
 import math, random
 
 
-t_length = 10
+def main(t_len):
+    result = {}
+    n = int(t_len) * 8
+    k = int(n/2) if n%2==0 else int(n/2)+1 
 
+    matrix = f.generate_matrix(n,k)
+    result["matrix"] = matrix
 
-n = t_length * 8
+    gv = f.gilbert_varshamov_distance(n,k)
+    result["gv"] = gv
 
-k = int(n/2) if n%2==0 else int(n/2)+1
+    return result
+    
 
-gv = f.gilbert_varshamov_distance(n,k)
-print('GV: ', gv)
+def create_code(t_len):
+    n = int(t_len) * 8
+    k = int(n/2) if n%2==0 else int(n/2)+1 
+    matrix = f.generate_matrix(n,k)
+    return matrix
 
-t = f.zeros(t_length)
-n_errors = int(math.floor(gv-1)/2)
-print("allowed error count:", n_errors)
-for i in range(n_errors):
-    t[random.randint(0,t_length-1)]+=1
+def gilbert_varshamov_dist(t_len,k):
+    n = t_len * 8
+    gv = f.gilbert_varshamov_distance(n,k)
+    return gv
 
-print("t: ",t)    
-if sum(t) > math.floor((gv-1)/2):
-    raise TypeError("Sum of T cant be more than (GV-1)/2")
-print(f"n: {n}")
-print(f"k: {k}")
-
-
-i= f.gen_inf_set_with_hints(t, n, k)
-print(i)
-
-
+# t_length = 4
+# n = t_length * 8
+# k = int(n/2) if n%2==0 else int(n/2)+1
+# gv = f.gilbert_varshamov_distance(n,k)
+# print('GV: ', gv)
+# t = f.zeros(t_length)
+# n_errors = int(math.floor(gv-1)/2)
 
--- src/server.py
+++ src/server.py
@@ -1,20 +1,32 @@
-from flask import Flask, render_template, request
-from func import generate_matrix, gen_random_codeword, gen_random_e, add_vectors, mask_matrix, mask_vector, gen_information_set
+from flask import Flask, render_template, request, url_for, redirect
+import os, main
 
 app = Flask(__name__, template_folder="templates")
+
+T_SIZE = 0
 
 @app.route('/')
 def home():
     return "MAIN PAGE"
 
 
-@app.route('/gen-code', methods=["POST","GET"])
-def generate():
+@app.route('/generate-code', methods=["POST","GET"])
+def generate_code():
     if request.method == "POST":
         form = request.form
-        return render_template('matrix.html')
+        os.environ["T_SIZE"] = form["t_size"]
+        return redirect(url_for('generate_inputs'))
     else:
         return render_template('index.html')
+    
+@app.route('/generate-inputs', methods=["POST", "GET"])
+def generate_inputs():
+    if request.method== "GET":
+        t_size = os.environ["T_SIZE"]
+        data = main.main(t_size)
+        return render_template('gen_inputs.html', data=data)
+    else:
+        return "under development"
 
 
 if __name__ == "__main__":
